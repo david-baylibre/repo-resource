69314e71ff5b7ccd4e99f84de3e0d0d1abec08a9
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2022-07-13 10:27:38 +0200

    in_: fix stubbed function prototype
    As [1] states:
    > The in script is passed a destination directory as command line argument
    > $1, and is given on stdin the configured source and a precise version of
    > the resource to fetch.
    
    Fix the stubbed in_() implementation to take argv[1] as argument as
    well.
    
    This allows us to run a "dummy" get.
    
    Also add some of the documentation inside the in_.py file for easier access.
    
    [1] https://concourse-ci.org/implementing-resource-types.html#resource-in
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- repo_resource/in_.py
+++ repo_resource/in_.py
@@ -8,12 +8,27 @@ import json
 import sys
 
 
-def in_(instream):
-    return []
+def in_(instream, dest_dir='.'):
+    """Fetch the resource and place it in dest_dir
+
+    We receive on stdin the configured source
+    and a precise version of the resource to fetch.
+
+    If the desired resource version is unavailable (for example, if it was
+    deleted), the script must exit with error.
+
+    The script must emit the fetched version, and may emit metadata as a list
+    of key-value pairs. This data is intended for public consumption and will
+    make it upstream, intended to be shown on the build's page.
+    """
+    payload = json.load(instream)
+    version = payload['version']['sha256']
+
+    return {"version": {"sha256": version}}
 
 
 def main():
-    print(json.dumps(in_(sys.stdin)))
+    print(json.dumps(in_(sys.stdin, sys.argv[1])))
 
 
 if __name__ == '__main__':
